a9b42aab52b851a3871ba91b1c461a997233ddda
Kerasiya Vishnu <vishnuk@ziguts.com>
2023-11-24 14:27:44 +0000

    test lint-staged 2

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 function login(username: string): string {
-  // console.log(username)
   const user = {
-    name:'barca'
-  }; const name = user['name'];
+    name: "barca",
+  };
+  const name = user["name"];
 
   return username + name;
 }
