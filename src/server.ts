f44afdb837ea2f1fc439e438fcea792ed5ba8ccb
Kerasiya Vishnu <vishnuk@ziguts.com>
2023-11-25 22:38:00 +0000

    add express js

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
 import { Config } from "./config";
+import app from "./app";
 
-console.log(Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+startServer();
