3c24f4d3f7b99459698a671385dfadf6497b4345
Kerasiya Vishnu <vishnuk@ziguts.com>
2023-11-26 16:39:43 +0000

    add logger

---
+++ src/config/logger.ts
@@ -0,0 +1,28 @@
+import winston from "winston";
+import { Config } from "./index";
+const { format } = winston;
+
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { serviceName: "auth-service" },
+  transports: [
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "error.log",
+      level: "error",
+      silent: Config.NODE_ENV === "test",
+    }),
+    new winston.transports.File({
+      dirname: "logs",
+      filename: "combined.log",
+      level: "info",
+      silent: Config.NODE_ENV === "test",
+    }),
+    new winston.transports.Console({
+      level: "info",
+      format: format.combine(format.timestamp(), format.json()),
+      silent: Config.NODE_ENV === "test",
+    }),
+  ],
+});
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import { Config } from "./config";
 import app from "./app";
+import { logger } from "./config/logger";
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
     app.listen(PORT, () => {
-      console.log(`Listening on port ${PORT}`);
+      logger.info(`Listening on port ${PORT}`);
     });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(error.message);
+      setTimeout(() => {
+        process.exit(1);
+      }, 1000);
+    }
   }
 };
 startServer();
