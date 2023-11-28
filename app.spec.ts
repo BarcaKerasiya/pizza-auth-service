e3ccd6926463c2d6f99c22a42038c57174cc2c6d
Kerasiya Vishnu <vishnuk@ziguts.com>
2023-11-28 22:14:37 +0000

    add test setup

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import app from "./src/app";
+import { calculateDiscount } from "./src/config/utils";
+import request from "supertest";
+
+describe("app", () => {
+  it("shoud calculate the discount price", () => {
+    const result = calculateDiscount(100, 10);
+    expect(result).toBe(10);
+  });
+
+  it("should return 200 status", async () => {
+    const response = await request(app).get("/").send();
+    expect(response.statusCode).toBe(200);
+  });
+});
